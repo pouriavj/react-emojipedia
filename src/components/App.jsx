64bf132dd648b70b235ffac201a49fb9c745664a
Pouria <108181964+pouriavj@users.noreply.github.com>
2025-10-27 00:37:11 +0330

    Update to arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,6 @@ import React from "react";
 import Entry from "./Entry";
 import emojipedia from "../emojipedia";
 
-function createEntry(emoji) {
-  return (
-    <Entry
-      key={emoji.id}
-      name={emoji.name}
-      emoji={emoji.emoji}
-      meaning={emoji.meaning}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
@@ -20,7 +9,16 @@ function App() {
         <span>emojipedia</span>
       </h1>
 
-      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map(emoji => (
+          <Entry
+            key={emoji.id}
+            name={emoji.name}
+            emoji={emoji.emoji}
+            meaning={emoji.meaning}
+          />
+        ))}
+      </dl>
     </div>
   );
 }
