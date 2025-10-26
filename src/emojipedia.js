96bd2fbd1cc814a54bd5ce54d9316604a015e552
Pouria <108181964+pouriavj@users.noreply.github.com>
2025-10-26 11:22:29 +0330

    Initial Commit

---
+++ src/emojipedia.js
@@ -0,0 +1,48 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+   {
+    id: 3,
+    emoji: "😃",
+    name: "Smiling Face With Open Mouth",
+    meaning:
+      "The grinning face and big eyes emoji symbolizes positive vibes, happy laughter and enthusiasm."
+  },
+  {
+    id: 3,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:
+      "A heavenly smile is shown by the halo emoji, which symbolizes innocence and goodness. "
+  },
+  {
+    id: 3,
+    emoji: "🙃",
+    name: "Upside-Down Face",
+    meaning:
+      "I'm not serious! The inverted smiling face indicates that you are not serious or have a humorous undertone. It is suitable for ambiguous statements, ironic remarks or joking comments."
+  },
+];
+
+
+
+export default emojipedia;
